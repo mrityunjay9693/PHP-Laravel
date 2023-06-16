0bfaf05c77dba712478ac2359b9b0a7452a7971b
mrityunjay9693 <mkaykumar123@gmail.com>
2023-06-16 17:18:10 +0530

    basic

---
+++ CommandLine/Addition.php
@@ -0,0 +1,7 @@
+<?php
+//print "Enter 1st number: ";
+$a = readline("Enter 1st number: ");
+//print "Enter 2nd number: ";
+$b = readline("Enter 2nd number: ");
+$c = $a + $b;
+echo "The sum is $c\n";  
