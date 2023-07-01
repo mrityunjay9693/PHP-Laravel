edcf919e84b4cf5ec62b5eca25556a2fbcdb5a8a
mrityunjay9693 <mkaykumar123@gmail.com>
2023-07-01 16:56:19 +0530

    Number is positive or negative or Zero

---
+++ CommandLine/Decision Making-Branching Control Statement/PositiveNegative.php
@@ -0,0 +1,23 @@
+<?php
+//A PHP program to take input from user and 
+//Print, is the input is "positive", "negative", or "zero"
+
+$number = readline("Enter a number: ");
+
+if($number>0){
+
+    echo "$number is Positive";
+
+}
+
+if($number<0){
+
+    echo "$number is Negative";
+
+}
+
+if($number == 0){
+    
+    echo "The entered number is Zero" ."\n";
+
+}
