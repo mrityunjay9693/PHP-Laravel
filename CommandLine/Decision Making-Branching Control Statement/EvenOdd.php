d3f8228089c2f4c5c34c6054f1e3ddeee9c602c1
mrityunjay9693 <mkaykumar123@gmail.com>
2023-07-01 22:20:51 +0530

    program to find the number is even or odd

---
+++ CommandLine/Decision Making-Branching Control Statement/EvenOdd.php
@@ -0,0 +1,15 @@
+<?php
+//Write a program in php to read a number as input from user and 
+//print it, is it even or odd.
+
+//$EvenOdd = array("even","odd");
+$number = readline("Enter number: ");
+
+if($number%2==0){
+    echo "$number is even" ."\n";
+}
+else{
+    echo "$number is odd" ."\n";
+} 
+
+//echo "$number is : array($number%2)" ."\n";
