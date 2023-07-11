089a68d17a49af38df32f1138d823df236b91b7e
mrityunjay9693 <56473482+mrityunjay9693@users.noreply.github.com>
2023-07-11 23:02:10 +0530

    print middle value if input values are distict integer

--- CommandLine/Decision Making-Branching Control Statement/distinctInteger.php
+++ CommandLine/Decision Making-Branching Control Statement/distinctInteger.php
@@ -1,4 +1,5 @@
 <?php
+
 /* 
 Write a php program to read three value from user and
 print 
@@ -9,8 +10,10 @@ $num2 = readline("Enter 2nd number: ");
 $num3 = readline("Enter 3rd number: ");
 
 if($num1 != $num2 && $num1 != $num3 && $num2 != $num3){
+
     echo "Middle value: $num2" ."\n";
 }
 else{
+
     echo "Please enter distinct values." ."\n";
 }
